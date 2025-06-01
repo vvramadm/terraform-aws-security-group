e73080d868281e0bd961bdb148a3ec2dacbb60b1
vvramadm <60600569+vvramadm@users.noreply.github.com>
2025-06-02 00:47:04 +0530

    Update main.tf

--- terraform-aws-security-group/main.tf
+++ terraform-aws-security-group/main.tf
@@ -1,22 +1,23 @@
-# expense-dev-mysql
 resource "aws_security_group" "main" {
-  name        = local.sg_final_name
-  description = var.sg_description
-  vpc_id      = var.vpc_id
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    name = local.sg_final_name
+    tags = merge(
 
-  tags = merge(
-    var.common_tags,
-    var.sg_tags,
-    {
-        Name = local.sg_final_name
+        var.common_tags,
+
+        {
+            Name= local.sg_final_name
+        }
+
+    )
+
+    egress  {
+
+        from_port = 0
+        to_port   = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
-  )
+  
 }
