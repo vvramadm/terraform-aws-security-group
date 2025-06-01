f5ad996e548b4da429e595fb33a208cdd384eb3b
vvramadm <60600569+vvramadm@users.noreply.github.com>
2025-06-02 00:48:14 +0530

    Update variables.tf

--- terraform-aws-security-group/variables.tf
+++ terraform-aws-security-group/variables.tf
@@ -1,27 +1,26 @@
 variable "project_name" {
-
+  
 }
 
 variable "environment" {
-
+  
 }
-
-variable "sg_name"{
-
+variable "sg_name" {
+  default = {}
 }
-
-variable "sg_description" {
-    
+variable "sg_tags" {
+  default = {}
 }
-
 variable "vpc_id" {
 
 }
 
-variable "common_tags" {
-    type = map
-}
+variable "common_tags"{
+default = {
+ Project = "expense"
+ Environment = "dev"
+ Terraform = "true"
 
-variable "sg_tags" {
-    default = {}
 }
+}
+  
